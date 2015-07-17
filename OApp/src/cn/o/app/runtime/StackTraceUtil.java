2c82e0204b067f572cb6e9cb341a5adce99f3aa1
杨文华 <lounien@qq.com>
2015-07-17 14:02:40 +0800

    Using Throwable.getStackTrace() to replace Thread.currentThread().getStackTrace() because of the following bug
    Android environment stack trace:
    dalvik.system.VMStack.getThreadStackTrace
    java.lang.Thread.getStackTrace
    cn.o.app.runtime.StackTraceUtil.getCallerElement
    ... ....
    
    Java standard environment stack trace:
    java.lang.Thread.getStackTrace
    cn.o.app.runtime.StackTraceUtil.getCallerElement
    ... ...

--- OApp/src/cn/o/app/runtime/StackTraceUtil.java
+++ OApp/src/cn/o/app/runtime/StackTraceUtil.java
@@ -1,13 +1,51 @@
 package cn.o.app.runtime;
 
+/**
+ * Stack trace utility of framework
+ * 
+ * Using Throwable.getStackTrace() to replace
+ * Thread.currentThread().getStackTrace() because of the following bug
+ * 
+ * Android environment stack trace:
+ * 
+ * dalvik.system.VMStack.getThreadStackTrace
+ * 
+ * java.lang.Thread.getStackTrace
+ * 
+ * cn.o.app.runtime.StackTraceUtil.getCallerElement
+ * 
+ * ... ....
+ * 
+ * 
+ * Java standard environment stack trace:
+ * 
+ * java.lang.Thread.getStackTrace
+ * 
+ * cn.o.app.runtime.StackTraceUtil.getCallerElement
+ * 
+ * ... ...
+ * 
+ */
 public class StackTraceUtil {
 
+	/**
+	 * Get stack trace element for the current method
+	 * 
+	 * @return
+	 */
 	public static StackTraceElement getCurrentElement() {
-		return Thread.currentThread().getStackTrace()[3];
+		StackTraceElement[] elements = new Throwable().getStackTrace();
+		return 1 < elements.length ? elements[1] : elements[elements.length - 1];
 	}
 
+	/**
+	 * Get stack trace element for the method who call you
+	 * 
+	 * @return
+	 */
 	public static StackTraceElement getCallerElement() {
-		return Thread.currentThread().getStackTrace()[4];
+		StackTraceElement[] elements = new Throwable().getStackTrace();
+		return 2 < elements.length ? elements[2] : elements[elements.length - 1];
 	}
 
 }
