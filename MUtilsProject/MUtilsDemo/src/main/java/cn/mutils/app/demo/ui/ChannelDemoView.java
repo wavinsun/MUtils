0591029542436859868382af1cdfa9496954224b
杨文华 <wavinsun@qq.com>
2015-12-18 10:08:03 +0800

    Move Mutils and MUtilsDemo to gradle.

---
+++ MUtilsProject/MUtilsDemo/src/main/java/cn/mutils/app/demo/ui/ChannelDemoView.java
@@ -0,0 +1,37 @@
+package cn.mutils.app.demo.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import cn.mutils.app.core.annotation.res.FindViewById;
+import cn.mutils.app.core.annotation.res.SetContentView;
+import cn.mutils.app.demo.R;
+import cn.mutils.app.ui.StateView;
+import cn.mutils.app.util.AppUtil;
+
+@SetContentView(R.layout.view_channel)
+public class ChannelDemoView extends StateView {
+
+    @FindViewById(R.id.log)
+    protected TextView mLog;
+
+    public ChannelDemoView(Context context) {
+        super(context);
+    }
+
+    public ChannelDemoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ChannelDemoView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mLog.setText(AppUtil.getAppMetaData(getContext(),"UMENG_CHANNEL"));
+    }
+}
