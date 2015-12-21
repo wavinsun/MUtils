c91124fd97db831085f777405dbd34069a416247
杨文华 <wavinsun@qq.com>
2015-12-21 19:07:30 +0800

    Optimize build script.

--- MUtilsProject/build.bat
+++ MUtilsProject/build.bat
@@ -1,4 +1,7 @@
 @echo off&title Build Android Application
+move gradle\wrapper\gradle-wrapper.properties gradle\wrapper\gradle-wrapper.properties.bak
+copy gradle\wrapper\gradle-wrapper-local.properties gradle\wrapper\gradle-wrapper.properties
 call gradlew clean
 call gradlew assembleRelease
+move gradle\wrapper\gradle-wrapper.properties.bak gradle\wrapper\gradle-wrapper.properties
 echo. & pause
